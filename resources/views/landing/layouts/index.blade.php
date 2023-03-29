4c40f1648facea264049c33346bafced088379fc
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2023-03-29 15:37:36 +0430

    landing page customization done

--- resources/views/landing/layouts/index.blade.php
+++ resources/views/landing/layouts/index.blade.php
@@ -19,15 +19,15 @@
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="{{ asset('assets_landing/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets_landing/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets_landing/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets_landing/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets_landing/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets_landing/vendor/aos/aos.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="{{ asset('assets_landing/css/main.css') }}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Logis - v1.3.0
@@ -153,15 +153,15 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="{{ asset('assets_landing/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets_landing/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+  <script src="{{ asset('assets_landing/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset('assets_landing/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('assets_landing/vendor/aos/aos.js') }}"></script>
+  <script src="{{ asset('assets_landing/vendor/php-email-form/validate.js') }}"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="{{ asset('assets_landing/js/main.js') }}"></script>
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [landingController::class, 'index']);
-
+// Admin Section Routes
 Route::group(['prefix' => 'admin'], function(){
     Route::get('/',[DashboardController::class, 'index'])->name('admin');
     Route::resource('about', AboutController::class);
 });
 
+
+// Auth Section Routes
 Route::get('/login',[LoginController::class, 'index']);
+
+
+// Landing Section Routes
+Route::get('/', [landingController::class, 'index']);
